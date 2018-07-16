abcc593939e1ad99f00c9b02a001ce9f79fa51ad
Soner Paycı <sonerpayci@hotmail.com>
2018-07-16 18:11:48 +0300

    first draft created

---
+++ src/components/FooterHero.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="hero-foot">
+    <footer class="footer">
+      <div class="mobile-footer">
+        <div class="made-by" :class="{'hide': isOpenedInIFrame}">Made by <a href="https://github.com/djirdehh" target="_blank">Hassan Djirdeh</a> | <icon name="calendar" height="12"></icon> 2017</div>
+        <p class="wallet-tag">BTC <span>1cV9YMjpweZyJ17DJbTa1WELs4BjdmUMp</span></p>
+        <p class="wallet-tag">ETH <span >0x5af8dfc441d4068f705372f461a85aba49aa5f46</span></p>
+      </div>
+      <div class="desktop-footer" :class="{'hide': isOpenedInIFrame}">
+        <a href="https://chrome.google.com/webstore/search/cryptovue" target="_blank" class="button is-medium"><img :src="`/static/chrome-webstore.png`" style="height: 50px"/></a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footerHero',
+  props: {},
+  data () {
+    return {
+      isOpenedInIFrame: false
+    }
+  },
+  created () {
+    if (window.self !== window.top) {
+      this.isOpenedInIFrame = true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$small: 590px;
+$medium: 768px;
+$large: 1024px;
+
+.footer {
+  background-color: transparent;
+  padding: 3rem 1.5rem 0rem;
+  position: relative;
+
+  @media screen and (max-width: $small) {
+    padding-top: 60px;
+  }
+
+  .mobile-footer {
+    display: none;
+    font-size: 15px;
+    font-weight: 400;
+    position:absolute;
+    bottom: 15px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+
+    a {
+      color: #fd6721;
+    }
+
+    @media screen and (max-width: $small) {
+      display: block;
+    }
+
+    .wallet-tag {
+      font-size: 9px;
+      font-weight: 600;
+
+      span {
+        color: #00d1b2;
+      }
+    }
+  }
+
+  .desktop-footer {
+    display: block;
+
+    .button {
+      float: right;
+      padding: 10px;
+      margin-bottom: 10px;
+    }
+
+    @media screen and (max-width: $medium) {
+      display: none;
+    }
+  }
+}
+
+.hide {
+  display: none !important;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import {get}  from 'axios'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+//  const getUrl = 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+const cryptoCurrencyData = require('./cryptocurrency-data.json')
+
+export const store = {
+  //cryptoCompareUrl: 'https://min-api.cryptocompare.com/data/top/totalvol?tsym=USD&limit=500',
+  cryptoCompareUrl: 'https://masternodes.pro/apiv2/coin/stats/',
+  state: {
+    totalMarketCapUSD: 0,
+    cryptoCurrencies: []
+  },
+  getCryptoCurrencies: function () {
+    let coinFilter = ["Dash","Pivx","Bitg","xzc","pac","bsd","SIB"];  //Syscoin- Xsn- Phr-
+      for (let i = 0; i<coinFilter.length; i++) {
+        get(this.cryptoCompareUrl + coinFilter[i]).then((response) => {
+          this.state.cryptoCurrencies.push(response.data);
+        });
+      }
+      console.log( this.state.cryptoCurrencies);
+      this.state.cryptoCurrencies.forEach( (cryptoCurrency) => {
+      });
+  },
+  getDifferenceInChange (cryptoCurrency) {
+    cryptoCurrency.positivePercentChange = !(cryptoCurrency.stats.cmc.percent_change_24h.indexOf('-') > -1)
+    cryptoCurrency.percentChange24h = cryptoCurrency.stats.cmc.percent_change_24h.replace(/^-/, '')
+  },
+  addImageAndDescription(cryptoCurrency) {
+    cryptoCurrency.id =  cryptoCurrency.stats.cmc.id in cryptoCurrencyData ? cryptoCurrency.stats.cmc.id : undefined;
+    cryptoCurrency.description = cryptoCurrencyData[cryptoCurrency.id].description;
+    cryptoCurrency.website = cryptoCurrency.href;
+    cryptoCurrency.paper = cryptoCurrency.mpndata.sites.coin_ann;
+    cryptoCurrency.github = cryptoCurrency.mpndata.sites.coin_github;
+    cryptoCurrency.positivePercentChange = !(cryptoCurrency.stats.cmc.percent_change_24h.indexOf('-') > -1);
+    cryptoCurrency.percentChange24h = cryptoCurrency.stats.cmc.percent_change_24h.replace(/^-/, '');
+  },
+  getTotalMarketCapUSD() {
+    const getUrl = 'https://api.coinmarketcap.com/v1/global/'
+    axios.get(getUrl).then((response) => {
+      const globalData = response.data
+      this.state.totalMarketCapUSD = globalData.total_market_cap_usd
+    })
+  }
+}
